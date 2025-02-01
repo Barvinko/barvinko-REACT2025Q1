468848091452022c5d8297a5fb91072bb91a9060
Barvinko <vlad.barvinko@gmail.com>
2025-02-01 14:47:11 +0200

    feat: implement character card display

--- src/components/Main/CardList/CardList.tsx
+++ src/components/Main/CardList/CardList.tsx
@@ -1,11 +1,22 @@
 import { Component, ReactNode } from 'react';
+import { Card } from './Card/Card';
+import { Character } from '../../../types/types';
 import './CardList.css';
 
-export class CardList extends Component {
+interface CardListProps {
+  dataCharacters: Character[];
+}
+
+export class CardList extends Component<CardListProps> {
   render(): ReactNode {
     return (
-      <section className="class-list">
-        <h2 className="class-list__title">Result:</h2>
+      <section className="card-list">
+        <h2 className="card-list__title">Result:</h2>
+        <div className="card-list__cards">
+          {this.props.dataCharacters.map((character, index) => (
+            <Card key={index} {...character} />
+          ))}
+        </div>
       </section>
     );
   }
