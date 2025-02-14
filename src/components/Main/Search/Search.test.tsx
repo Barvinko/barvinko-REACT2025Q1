f8e9f782565d75c62d5763fae8f9ed58d58c5f9f
Barvinko <vlad.barvinko@gmail.com>
2025-02-14 22:04:36 +0200

    refactor: simplify Search tests by removing unused props and commented code

--- src/components/Main/Search/Search.test.tsx
+++ src/components/Main/Search/Search.test.tsx
@@ -7,11 +7,7 @@ test('renders Search component and handles input', () => {
   const mockNameRequest = vi.fn();
   render(
     <MemoryRouter>
-      <Search
-        page={1}
-        nameRequest={mockNameRequest}
-        handlePageChange={vi.fn()}
-      />
+      <Search nameRequest={mockNameRequest} />
     </MemoryRouter>
   );
 
@@ -27,7 +23,7 @@ test('renders Search component and handles input', () => {
 test('displays error message for invalid input', () => {
   render(
     <MemoryRouter>
-      <Search page={1} nameRequest={vi.fn()} handlePageChange={vi.fn()} />
+      <Search nameRequest={vi.fn()} />
     </MemoryRouter>
   );
 
@@ -37,40 +33,3 @@ test('displays error message for invalid input', () => {
     screen.getByText('Only letters, numbers, spaces, and hyphens are allowed.')
   ).toBeInTheDocument();
 });
-
-// test('loads initial value from local storage', () => {
-//   localStorage.setItem(
-//     'Barvinko-classComponents__name',
-//     JSON.stringify('Leia')
-//   );
-//   const mockNameRequest = vi.fn();
-//   render(
-//     <MemoryRouter>
-//       <Search
-//         page={1}
-//         nameRequest={mockNameRequest}
-//         handlePageChange={vi.fn()}
-//       />
-//     </MemoryRouter>
-//   );
-//   const input = screen.getByPlaceholderText('Name...');
-//   expect(input).toHaveValue('Leia');
-// });
-
-// test('calls nameRequest on mount with localName', () => {
-//   const mockNameRequest = vi.fn();
-//   localStorage.setItem(
-//     'Barvinko-classComponents__name',
-//     JSON.stringify('Leia')
-//   );
-//   render(
-//     <MemoryRouter>
-//       <Search
-//         page={1}
-//         nameRequest={mockNameRequest}
-//         handlePageChange={vi.fn()}
-//       />
-//     </MemoryRouter>
-//   );
-//   expect(mockNameRequest).toHaveBeenCalledWith('Leia', 1);
-// });
