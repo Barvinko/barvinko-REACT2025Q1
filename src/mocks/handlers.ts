3ab6d9574d44ddb8b2c5ec4112efe7848816a0e4
Barvinko <vlad.barvinko@gmail.com>
2025-02-23 18:29:22 +0200

    refactor: delete unnecessary additional MSV server implementation

--- src/mocks/handlers.ts
+++
@@ -1,7 +0,0 @@
-import { http } from 'msw';
-
-export const handlers = [
-  http.get('https://swapi.dev/api/people/', (req, res, ctx) => {
-    return res(ctx.json({ results: [{ name: 'Luke Skywalker' }], count: 1 }));
-  }),
-];
